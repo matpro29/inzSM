cc72af08e454a33d979aecdd75c40a55e354c618
Sylwia <sylwebik@gmail.com>
2018-11-06 16:05:04 +0100

    06.11.2018 , dziwne u mnie działa

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -4,6 +4,7 @@ import 'owl.carousel/dist/owl.carousel';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 
+<<<<<<< Updated upstream
 
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
@@ -35,3 +36,88 @@ $(document).ready(function(){
 
 
 
+=======
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        items:1,
+        nav:false,
+        dots:true,
+        autoplay:false,
+        autoplayTimeout:5000,
+    });
+
+    function stickyHeader() {
+        if ($(this).scrollTop() > 50) {
+
+            $('.site-header').addClass('sticky-header');
+
+        } else {
+
+            $('.site-header').removeClass('sticky-header');
+
+        }
+
+    }
+
+    function windowScroll() {
+
+        $(window).on('scroll', function () {
+            stickyHeader();
+        });
+    }
+
+    windowScroll();
+    stickyHeader();
+
+    $(".site-header").on("click", "a", function (event) {
+
+        var $href = $(this).attr('href').replace('/','');
+
+        var $anchor = $($href).offset();
+
+        if (typeof $anchor !== 'undefined') {
+
+            $('html, body').animate({
+
+                scrollTop: $anchor.top
+
+            }, 1000);
+
+            return false;
+
+
+
+        }
+
+    });
+
+
+
+    $('.main-navigation a').on('click', function (e) {
+
+        $(this).closest('.main-navigation').removeClass('toggled');
+
+        $(this).closest('.main-navigation').find('.hamburger').removeClass('hamburger-open');
+
+        $(this).parent().addClass('active-menu-item');
+
+        $(this).parent().siblings().removeClass('active-menu-item');
+
+    });
+
+
+
+    //hamburger
+
+    $("button.hamburger").on("click", function () {
+
+        $(this).toggleClass("hamburger-open");
+
+        // $(this).next("ul").toggleClass("hidden");
+
+    });
+
+});
+>>>>>>> Stashed changes
