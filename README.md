ada85bde3ff859d54a2c35cb73a2b92e075c393d
Sylwia <sylwebik@gmail.com>
2018-11-09 18:49:56 +0100

    09.11.2018 kijowe ale jest

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ Należy wykonać następujące polecenia:
   
   * yarn encore dev
 
+  * yarn add owl.carousel jquery
+  
   * W .env zamień "DATABASE_URL=mysql://db_user:db_password@127.0.0.1:3306/db_name"
 
   * php bin/console doctrine:database:create
--- assets/js/layout.js
+++ assets/js/layout.js
@@ -3,8 +3,8 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/owl.carousel';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
+import 'owl.carousel/'
 
-<<<<<<< Updated upstream
 
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
@@ -27,97 +27,3 @@ $(document).ready(function(){
 });
 
 
-
-
-
-
-
-
-
-
-
-=======
-$(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        items:1,
-        nav:false,
-        dots:true,
-        autoplay:false,
-        autoplayTimeout:5000,
-    });
-
-    function stickyHeader() {
-        if ($(this).scrollTop() > 50) {
-
-            $('.site-header').addClass('sticky-header');
-
-        } else {
-
-            $('.site-header').removeClass('sticky-header');
-
-        }
-
-    }
-
-    function windowScroll() {
-
-        $(window).on('scroll', function () {
-            stickyHeader();
-        });
-    }
-
-    windowScroll();
-    stickyHeader();
-
-    $(".site-header").on("click", "a", function (event) {
-
-        var $href = $(this).attr('href').replace('/','');
-
-        var $anchor = $($href).offset();
-
-        if (typeof $anchor !== 'undefined') {
-
-            $('html, body').animate({
-
-                scrollTop: $anchor.top
-
-            }, 1000);
-
-            return false;
-
-
-
-        }
-
-    });
-
-
-
-    $('.main-navigation a').on('click', function (e) {
-
-        $(this).closest('.main-navigation').removeClass('toggled');
-
-        $(this).closest('.main-navigation').find('.hamburger').removeClass('hamburger-open');
-
-        $(this).parent().addClass('active-menu-item');
-
-        $(this).parent().siblings().removeClass('active-menu-item');
-
-    });
-
-
-
-    //hamburger
-
-    $("button.hamburger").on("click", function () {
-
-        $(this).toggleClass("hamburger-open");
-
-        // $(this).next("ul").toggleClass("hidden");
-
-    });
-
-});
->>>>>>> Stashed changes
